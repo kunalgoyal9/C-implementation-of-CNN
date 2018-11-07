d0ffb44445225c12d40bd23311399e9c5c60bb3f
kunalgoyal9 <kunalgoyal.goyal9@gmail.com>
2018-11-07 12:35:03 +0530

    tensor

---
+++ point.h
@@ -0,0 +1,20 @@
+
+// point having x,y,z dimensions
+
+struct point{
+	int x,y,z;
+	
+	bool operator==(point other){
+		if(this->x == other.x and this->y == other.y and this->z == other.z){
+			return true;
+		}
+		else return false;
+	}
+	
+	void operator=(point other){
+		x = other.x, y = other.y, z = other.z;
+	}
+};
+
+// size of a tensor
+using tdsize = point;
--- tensor.cpp
+++ tensor.cpp
@@ -1,11 +1,13 @@
 #include<bits/stdc++.h>
+#include"point.h"
 
 using namespace std;
 
+//template for tensor
 template<class T>
 class tensor{
 	public:
-		int nx,ny,nz;
+		tdsize size;
 		T *data;
 		
 		tensor(){
@@ -14,15 +16,86 @@ class tensor{
 		}
 		
 		tensor(int nx_, int ny_, int nz_){
-			this->nx = nx_;
-			this->ny = ny_;
-			this->nz = nz_;
+			this->size.x = nx_;
+			this->size.y = ny_;
+			this->size.z = nz_;
 			this->data = new T[nx_*ny_*nz_]; 
 		}
 		
+		tensor(const tensor& other){
+			memcpy(data, other.data, other.size.x*other.size.y*other.size.z);
+			this->size = other.size;
+		}
 		
+		tensor<T> operator+(const tensor<T>& other){
+			assert(other.size == size);
+			tensor<T> copy(other);
+			for(int i=0; i<size.x*size.y*size.z; i++){
+				copy.data[i] += this->data[i];
+			}
+			return copy;
+		}
+		
+		tensor<T> operator-(const tensor<T>& other){
+			assert(other.size == size);
+			tensor<T> copy(other);
+			for(int i=0; i<size.x*size.y*size.z; i++){
+				copy.data[i] -= this->data[i];
+			}
+			return copy;
+		}
+		
+		void operator=(const tensor<T>& other){
+			data = other.data;
+			size = other.size; 
+		}
+		
+		T& operator()(int x_, int y_, int z_){
+			return this->get(x_, y_, z_);
+		}
+		
+		T& get(int _x, int _y, int _z){
+			assert( _x >= 0 && _y >= 0 && _z >= 0 );
+			assert( _x < size.x && _y < size.y && _z < size.z );
+			
+			return data[
+			_z * (size.x * size.y) +
+				_y * (size.x) +
+				_x
+			];
+		}
+		
+		void copy_from( std::vector<std::vector<std::vector<T>>> data )
+		{
+			int z = data.size();
+			int y = data[0].size();
+			int x = data[0][0].size();
+
+			for ( int i = 0; i < x; i++ )
+				for ( int j = 0; j < y; j++ )
+					for ( int k = 0; k < z; k++ )
+						get( i, j, k ) = data[k][j][i];
+		}
+		
+		~tensor_t()
+		{
+			delete[] data;
+		}
+
 };
 
 int main(){
+	tensor<float> t(2,2,1);
+	for(int i=0; i<t.size.x*t.size.y*t.size.z; i++){
+		t.data[i] = 10;
+	}
+	tensor<float> tt(2,2,1);
+	
+	tt = tt + t;
+	cout<<tt.data[0];
+	//tensor<float> tt(t);
+	
+	
+	//cout<<t.data[1];
 	return 0;
 }
